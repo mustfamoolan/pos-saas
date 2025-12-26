b28abe496714ba4d52a92a4d4db8f90f2ef37e10
mustfamoolan <almoq3m@gmail.com>
2025-12-26 19:30:18 +0300

    Fix: Add headings key protection in AcnooWebSettingController

--- app/Http/Controllers/Admin/AcnooWebSettingController.php
+++ app/Http/Controllers/Admin/AcnooWebSettingController.php
@@ -15,6 +15,12 @@ class AcnooWebSettingController extends Controller
     public function index()
     {
         $page_data = get_option('manage-pages');
+        
+        // Ensure page_data has headings key
+        if (!isset($page_data['headings']) || !is_array($page_data['headings'])) {
+            $page_data['headings'] = [];
+        }
+        
         return view('admin.website-setting.manage-pages', compact('page_data'));
     }
 
